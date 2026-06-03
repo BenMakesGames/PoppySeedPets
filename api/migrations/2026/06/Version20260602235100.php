8578794a982196f27902fc2a0919bfd0fab855eb
Verm <32827189+Vermidia@users.noreply.github.com>
2026-06-03 00:13:51 -0500

    Fix class name - forgor to change it

--- api/migrations/2026/06/Version20260602235100.php
+++ api/migrations/2026/06/Version20260602235100.php
@@ -16,7 +16,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260514204856 extends AbstractMigration
+final class Version20260602235100 extends AbstractMigration
 {
     public function getDescription(): string
     {
