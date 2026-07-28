fe5d1e3db734932af81a50e244a7dc411913a208
Ben H <ben@benmakesgames.com>
2026-07-27 19:38:35 -0700

    bird stuff WIP

---
+++ api/migrations/2026/07/Version20260728023546.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260728023546 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // Bec de Corbin
+        $this->addSql(<<<EOSQL
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`, `physics`, `electronics`, `hacking`, `umbra`, `magic_binding`, `mining`) VALUES (537,0,0,2,0,0,0.23,0.775,-44,0.77,1,0,0,0,0,0,"brawl",0,0,0,0,NULL,NULL,0,0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,0,0,0,0,0,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1526,"Bec de Corbin","I mean, your pets did a great job and all, but is that the beak of a raven, or a falcon? It\'s kind of hard to say. I mean, look, I\'m just saying: for all I know this is _actually_ a Bec de Faucon! And you better not be trying to pass a Bec de Faucon as a Bec de Corbin!","tool/spear/bec-de-corbin",NULL,537,NULL,0,NULL,NULL,240,7,NULL,NULL,NULL,0,NULL,0,1) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1606,1526,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- item groups
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (43, 1526), (62, 1526);
+        EOSQL);
+    
+        // Raven's Beak
+        $this->addSql(<<<EOSQL
+        -- enchantment effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`, `physics`, `electronics`, `hacking`, `umbra`, `magic_binding`, `mining`) VALUES (539,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,NULL,0,0,0,0,343,108,1,0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,0,0,0,0,0,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- enchantment
+        INSERT INTO enchantment (`id`, `effects_id`, `name`, `is_suffix`, `aura_id`) VALUES (160,539,"of Ravens",1,NULL) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- tool effect
+        INSERT INTO item_tool (`id`, `stealth`, `nature`, `brawl`, `arcana`, `crafts`, `grip_x`, `grip_y`, `grip_angle`, `grip_scale`, `fishing`, `gathering`, `music`, `smithing`, `science`, `grip_angle_fixed`, `focus_skill`, `provides_light`, `protection_from_heat`, `always_in_front`, `is_ranged`, `when_gather_id`, `when_gather_also_gather_id`, `climbing`, `leads_to_adventure`, `prevents_bugs`, `attracts_bugs`, `can_be_nibbled`, `increases_pooping`, `dreamcatcher`, `is_grayscaling`, `social_energy_modifier`, `sex_drive`, `when_gather_prevent_gather`, `adventure_description`, `when_gather_apply_status_effect`, `when_gather_apply_status_effect_duration`, `physics`, `electronics`, `hacking`, `umbra`, `magic_binding`, `mining`) VALUES (538,0,0,3,0,0,0.23,0.775,-44,0.77,1,0,0,0,0,0,NULL,0,0,0,0,343,108,1,0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,0,0,0,0,0,0) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- the item itself!
+        INSERT INTO item (`id`, `name`, `description`, `image`, `use_actions`, `tool_id`, `food_id`, `fertilizer`, `plant_id`, `hat_id`, `fuel`, `recycle_value`, `enchants_id`, `spice_id`, `treasure_id`, `is_bug`, `hollow_earth_tile_card_id`, `cannot_be_thrown_out`, `museum_points`) VALUES (1527,"Raven\'s Beak",NULL,"tool/spear/ravens-beak",NULL,538,NULL,0,NULL,NULL,300,10,160,NULL,NULL,0,NULL,0,2) ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- grammar
+        INSERT INTO item_grammar (`id`, `item_id`, `article`) VALUES (1607,1527,"a") ON DUPLICATE KEY UPDATE `id` = `id`;
+
+        -- item groups
+        INSERT IGNORE INTO item_group_item (item_group_id, item_id) VALUES (43, 1527), (62, 1527);
+        EOSQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
